475b09980cd2ad0908a513572d356aa5fe90356c
korono36 <travismoore27@yahoo.com>
2020-02-22 14:48:44 -0500

    new commit

--- models/user.js
+++ models/user.js
@@ -1,16 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
-	username: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	password: {
-		type: String,
-		required: true
-	},
+
 	admin: {
 		type: Boolean,
 		default: false
@@ -18,3 +11,4 @@ const userSchema = new Schema({
 });
 
 module.exports = mongoose.model('User', userSchema);
+userSchema.plugin(passportLocalMongoose);
